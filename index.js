01cdb740aa582eea651504837c8f199cd8e9f5f9
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2021-08-18 13:23:47 +0000

    This commit implements a new function to return the coordinates

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ fetchMyIP((error, ip) => {
     console.log("It didn't work!" , error);
     return;
   }
-
   console.log('It worked! Returned IP:' , ip);
 });
+
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
+
--- iss.js
+++ iss.js
@@ -16,4 +16,23 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching coordinates for IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+
+module.exports = {
+  fetchMyIP,
+  fetchCoordsByIP
+};
